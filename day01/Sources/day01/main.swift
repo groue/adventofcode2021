f98cbdd13dd7b2e66341ad7d381be19c3aafaf91
Gwendal Roué <gr@pierlis.com>
2021-12-02 18:22:00 +0100

    day01, part 2

--- day01/Sources/day01/main.swift
+++ day01/Sources/day01/main.swift
@@ -1,5 +1,19 @@
+import Algorithms
+
 var numbers: [Int] = []
 while let line = readLine() {
     numbers.append(Int(line)!)
 }
-print(zip(numbers, numbers.dropFirst()).lazy.filter { $0 < $1 }.count)
+
+// Part 1
+let largerMeasurementCount = zip(numbers, numbers.dropFirst()).lazy
+    .filter { $0 < $1 }
+    .count
+print(largerMeasurementCount)
+
+// Part 2
+let triples = numbers.windows(ofCount: 3).map { $0.reduce(0, +) }
+let largerTripleCount = zip(triples, triples.dropFirst()).lazy
+    .filter { $0 < $1 }
+    .count
+print(largerTripleCount)
